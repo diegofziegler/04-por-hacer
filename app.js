dd9dd4408edf05a287da26eda23ad46d1aae3c6a
ext_diegoz <diego.f.ziegler@gmail.com>
2019-05-05 18:39:01 -0300

    actualizar

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 //const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
 const porHacer = require('./por-hacer/por-hacer');
 
-console.log(argv);
+//console.log(argv);
 
 let comando = argv._[0];
 
 switch( comando) {
     case 'crear':
         let tarea = porHacer.crear( argv.descripcion);
-        console.log( tarea );
-        
+        //console.log( tarea );
         break;
+
     case 'listar':
-        console.log(  `${comando} por hacer`);
+        //console.log(  `${comando} por hacer`);
+        let lista = porHacer.getListado();
+        console.log( `------- Tareas ${lista.length} -------`.blue);
+        for( let t of lista ) {
+            if(t.completado) {
+                console.log(
+                    `${t.descripcion} terminada: ${t.completado}`.green
+                );
+            } else {
+                console.log(
+                    `${t.descripcion} terminada: ${t.completado}`.red
+                );
+            }
+        }
+        console.log( `------- Final Tareas -------`.blue);
+
         break;
     case 'actualizar':
-        console.log(  `${comando} por hacer`);
+        //console.log(  `${comando} por hacer`);
+        let b= porHacer.actualizar( argv.descripcion, argv.completado);
+        console.log(`tarea actualizada: ${b}`);
         break;  
     default:
         console.log(  `${comando}: comando no reconocido`); 
--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 let listadoPorHacer = [];
 
+
+const cargar = () => {
+    /*
+    No usar ésta Verga, porque lo va a leer asyncrónico y me caga las operaciones que vienen luego...
+    fs.readFile("./db/data.json", (err, data) => {
+        if (err) throw new Error('No se pudo leer la lista de tareas', err);
+        listadoPorHacer = JSON.parse(data);
+        //console.log(listadoPorHacer);
+        //console.log(typeof(listadoPorHacer));
+        //console.log(listadoPorHacer[0]);
+      });    
+    */ 
+   listadoPorHacer = JSON.parse(fs.readFileSync("./db/data.json"));
+}
+
 const guardarDB = () => {
     let data = JSON.stringify(listadoPorHacer);
-
     fs.writeFile("./db/data.json", data, (err) => {
         if (err) 
             throw new Error('No se pudo grabar la lista de tareas', err);
@@ -11,20 +25,48 @@ const guardarDB = () => {
 }
 
 const crear = (descripcion) => {
+    cargar();
+
     let xhacer = {
         descripcion,
         completado: false,
     };
 
-    listadoPorHacer.push(xhacer);
+    
+    //console.log(listadoPorHacer.length);
+    let n = listadoPorHacer.push(xhacer);
+    //console.log(n);
     guardarDB();
     return xhacer;
 
 }
 
+const getListado = () => {
+    cargar();
+    return listadoPorHacer;
+}
+
+
+const actualizar = (descripcion, completado=true) => {
+    cargar();
+
+    let indice = listadoPorHacer.findIndex( tarea => {
+        return tarea.descripcion === descripcion;
+    });
+
+    if(indice >= 0) {
+        listadoPorHacer[indice].completado = completado;
+        guardarDB();
+    }
+
+    return (indice >= 0);
+
+}
+
+
 
 module.exports = {
 
-    crear
+    crear, getListado, actualizar
 
 }
